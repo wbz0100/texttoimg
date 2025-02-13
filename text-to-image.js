9e32ae692e0d8b4c5ee4e8846a6d074f8fcf6042
wbz0100 <91980289+wbz0100@users.noreply.github.com>
2025-02-13 16:28:03 +0900

    Update text-to-image.js

--- text-to-image.js
+++ text-to-image.js
@@ -53,7 +53,7 @@ app.get("/image.png", (req, res) => {
 
     // 캔버스 크기 설정
     const padding = 25;
-    const bottomPadding = 10; // 추가 여백
+    const bottomPadding = 20; // 추가 여백
     const canvasWidth = totalWidth + padding * 2;
     const canvasHeight = maxHeight + padding * 2 + bottomPadding;
     canvas.width = canvasWidth;
